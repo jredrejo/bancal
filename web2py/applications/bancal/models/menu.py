b151e68e9a82b11e970381a7590be9bbb6798c3e
José L. Redrejo Rodríguez <jredrejo@debian.org>
2013-09-27 08:53:26 +0200

    favicon y branding

--- web2py/applications/bancal/models/menu.py
+++ web2py/applications/bancal/models/menu.py
@@ -5,15 +5,15 @@
 ## Customize your APP title, subtitle and menus here
 #########################################################################
 
-response.logo = A(B('web',SPAN(2),'py'),XML('&trade;&nbsp;'),
+response.logo = A(B('Banco de Alimentos'),XML('&nbsp;Badajoz'),
                   _class="brand",_href="http://www.web2py.com/")
 response.title = request.application.replace('_',' ').title()
 response.subtitle = ''
 
 ## read more at http://dev.w3.org/html5/markup/meta.name.html
-response.meta.author = 'Your Name <you@example.com>'
-response.meta.description = 'a cool new app'
-response.meta.keywords = 'web2py, python, framework'
+response.meta.author = 'José L. Redrejo Rodríguez <jredrejo@itais.net>'
+response.meta.description = 'Gestión de Banco de Alimentos'
+response.meta.keywords = 'banco alimentos, banco, alimentos, caritas'
 response.meta.generator = 'Web2py Web Framework'
 
 ## your http://google.com/analytics id
@@ -137,4 +137,4 @@ def _():
          )]
 if DEVELOPMENT_MENU: _()
 
-if "auth" in locals(): auth.wikimenu() 
+if "auth" in locals(): auth.wikimenu()
